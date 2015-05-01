5409fe224e62f746f1999abcabdf0f1a5632e325
Grootstyr <git@grootstyr.eu>
2015-05-01 11:23:46 +0200

    Fix type error

--- src/main/java/database/DataRow.java
+++ src/main/java/database/DataRow.java
@@ -4,26 +4,25 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class DataRow {
-	
+
 	HashMap<String, String> columns;
-	
+
 	public DataRow() {
 		columns = new HashMap<String, String>();
 	}
-	
+
 	public int addColumn(String columnName, String columnElement) {
 		columns.put(columnName, columnElement);
-		
+
 		return 0;
 	}
-	
-	public Date getDate() {
+
+	public String getDate() {
 		String res = columns.get("date");
-		
-		if(res == null) {
-			throw new Exception("Date column not found");
+
+		if (res == null) {
+			return "not found";
 		}
-		
 		return res;
 	}
 
