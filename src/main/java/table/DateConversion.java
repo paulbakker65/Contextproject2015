7bf0b8eb5ca095a8e68deb266aacab9d413c79e9
Grootstyr <git@grootstyr.eu>
2015-05-06 13:23:35 +0200

    Fixed leap year bugs in Excel's data type

--- src/main/java/table/DateConversion.java
+++ src/main/java/table/DateConversion.java
@@ -8,21 +8,33 @@ public class DateConversion {
 	/**
 	 * Converts Excel Serial Date to a Java Date object.
 	 * 
-	 * @param excelSerial Excel Style Date in SerialDate format.
+	 * @param excelSerial Excel Style Date in SerialDate format. Double, date and time.
 	 * @return Date object in java.util.Date style
 	 */
 	public static Date fromExcelSerialToDate(double excelSerial) {
-		int nDays = (int)Math.floor(excelSerial);
+		int nDays = (int)excelSerial;
 		double partialDays = excelSerial - nDays;
 		int nMillis = (int)(partialDays * 24 * 60 * 60 * 1000);
 		
 		GregorianCalendar c = new GregorianCalendar();
 		c.set(1900, GregorianCalendar.JANUARY, 1, 0, 0, 0);
 		
-		c.add(GregorianCalendar.DAY_OF_YEAR, nDays);
+		c.add(GregorianCalendar.DATE, nDays);
 		c.add(GregorianCalendar.MILLISECOND, nMillis);
 		
-		return c.getTime();	
+		// Fix Excel's leap bugs
+		int leapDays = 0;
+		int year = c.get(GregorianCalendar.YEAR);
+		if (excelSerial >= 60) {
+			leapDays += 1;
+		}
+		if (year >= 2000) {
+			leapDays += ((int)(year / 1000) - 1);
+		}
+		c.add(GregorianCalendar.DATE, -leapDays);
+		
+		c.set(GregorianCalendar.MILLISECOND, 0);
+		return c.getTime();
 	}
 
 }
