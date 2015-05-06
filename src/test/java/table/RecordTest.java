c5101d16323d69c23fbf5690239469fb35940201
Jan van Rosmalen <jan@janvanrosmalen.nl>
2015-05-06 11:40:50 +0200

    Test fix

--- src/test/java/table/RecordTest.java
+++ src/test/java/table/RecordTest.java
@@ -1,27 +1,42 @@
 package table;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import input.Column;
+import input.StringColumn;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RecordTest {
 
+	ArrayList<Column> cols;
+	
+	@Before
+	public void setUp(){
+		cols = new ArrayList<Column>();
+		cols.add(new StringColumn("fruit"));
+		cols.add(new StringColumn("drink"));
+	}
+	
 	@Test
 	public void testContructor() {
 		Map<String,String> expected = new HashMap<String,String>();
 		expected.put("fruit", "banana");
 		expected.put("drink", "milk");
 		
-		assertEquals(expected,new Record(new String[] {"fruit","drink"},new String[]{"banana","milk"}));
+		assertEquals(expected,new Record(cols,new String[]{"banana","milk"}));
 		
 	}
 	
 	@Test
 	public void testToString(){
-		Record r = new Record(new String[] {"fruit","drink"},new String[]{"banana","milk"});
+		Record r = new Record(cols,new String[]{"banana","milk"});
 		String result = r.toString();
 		if (result.indexOf("banana")==-1||result.indexOf("milk")==-1){
 			fail(result);
