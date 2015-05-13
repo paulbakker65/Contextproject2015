6474f607f1fdbd342c18dcd51bc42c947beedb17
Jan van Rosmalen <jan@janvanrosmalen.nl>
2015-05-13 12:47:27 +0200

    Revert "Configuring checkstyle..."
    This reverts commit 850afb7d9453dc07f2ea57acf950deed6cba5f94.

--- src/main/java/export/Exporter.java
+++ src/main/java/export/Exporter.java
@@ -15,35 +15,32 @@ import table.*;
 
 public class Exporter {
 
-	public static void export(Table db, Writer writer, Settings settings)
-			throws IOException {
-		List<Column> temp = settings.getColumns(); // Get all the columns
+	public static void export(Table db, Writer writer, Settings settings) throws IOException {
+		List<Column> temp = settings.getColumns(); //Get all the columns
 		List<String> columns = new ArrayList<String>();
-		for (Column col : temp) {
+		for(Column col: temp) {
 			columns.add(col.getName());
 		}
 
-		CSVWriter csvwriter = new CSVWriter(writer, settings.getDelimiter()
-				.charAt(0));
-		csvwriter.writeNext(columns.toArray(new String[columns.size()])); // write
-																			// column
-																			// names
-		for (Record dr : db) {
-			csvwriter.writeNext(generateRow(dr, columns));
+		CSVWriter csvwriter = new CSVWriter(writer, settings.getDelimiter().charAt(0));
+		csvwriter.writeNext(columns.toArray(new String[columns.size()])); //write column names
+		for (Record dr : db){
+			csvwriter.writeNext(generateRow(dr,columns));
 		}
 		csvwriter.close();
 	}
-
-	public static String[] generateRow(Record dr, List<String> columns) {
+		
+	public static String[] generateRow(Record dr, List<String> columns){
 		List<String> items = new ArrayList<String>();
-		for (String column : columns) {
+		for (String column : columns){
 			Value value = dr.get(column);
-			if (value == null)
+			if (value==null)
 				items.add("");
 			else
 				items.add(value.toString());
 		}
 		return items.toArray(new String[items.size()]);
 	}
+	
 
 }
