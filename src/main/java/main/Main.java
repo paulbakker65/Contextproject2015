b7387ca0564710056f558d8bd273263f67b44b68
Julian Hols <jthols@users.noreply.github.com>
2015-05-13 14:11:35 +0200

    Updated Main: command line & GUI
    The main method now displays the GUI and reads the data from the dialog.
    It also supports command line arguments.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,49 +1,46 @@
 package main;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 import export.Exporter;
-import parsers.ColumnTypeMismatchException;
-import parsers.NumberValue;
-import parsers.Parser;
-import parsers.StringValue;
+
 import table.Table;
-import input.CSVReader;
-import input.Reader;
-import input.Settings;
 import input.WrongXMLException;
-import input.XMLReader;
 
 public class Main {
+	private static ArrayList<DataFile> files = new ArrayList<DataFile>();
+	private static File scriptFile;
+	private static File outputDir;
+	
+	public static void main(String[] args) throws IOException, URISyntaxException, WrongXMLException {
+		
+		parseCommandline(args);
+		
+		openGUI();
 
-	public static void main(String[] args) throws IOException, URISyntaxException, WrongXMLException, ColumnTypeMismatchException {
-		Settings settings_website = XMLReader.readXMLFile("/settings_website.xml");
-		Settings settings_statsensor = XMLReader.readXMLFile("/settings.xml");
-		Settings settings_hospital = XMLReader.readXMLFile("/settings_hospital.xml");
+		System.exit(0);	    
+	    
+		ArrayList<Table> tables = new ArrayList<Table>();
 		
-		Reader reader;
-		Parser parser;
+		for(DataFile f : files){
+			Table t = f.getParser().Parse(f.getReader());
+			tables.add(t);
+			System.out.println(t);
+		}		
+
+
+		//Read script & execute.
+
 		
-		reader = new CSVReader("medical/Q_ADMIRE_metingen_pagevisits_141214.csv", settings_statsensor.getDelimiter());
-		parser = new Parser(settings_website);
-		Table website = parser.parse(reader);
 		
-		reader = new CSVReader("medical/measured creatinine/ADMIRE_13.txt", settings_statsensor.getDelimiter());
-		parser = new Parser(settings_statsensor);
-		Table statsensor = parser.parse(reader);
-		
-		reader = new CSVReader("medical/Afspraken_geanonimiseerd.csv", settings_statsensor.getDelimiter());
-		parser = new Parser(settings_hospital);
-		Table hospital = parser.parse(reader);
-		
-//		System.out.println(website);
-//		System.out.println(statsensor);
-		//System.out.println(hospital);
-		
-		Table patientWebsite = website.getPatientByID(new StringValue("admire13"), "Login");
-		Table hospitalVisitPatient = hospital.getPatientByID(new NumberValue(13), "PatientID");
+		//DEMO START
+		Table patientWebsite = tables.get(0).getPatientByID("admire13", "Login");
+		Table hospitalVisitPatient = tables.get(2).getPatientByID("13", "PatientID");
+		Table statsensor = tables.get(1);
 		
 		System.out.println(patientWebsite);
 		System.out.println();
@@ -51,9 +48,80 @@ public class Main {
 		System.out.println();
 		System.out.println(hospitalVisitPatient);
 		
-		Exporter.export(patientWebsite, new FileWriter("output.csv"), settings_website);
-		Exporter.export(statsensor, new FileWriter("output2.csv"), settings_statsensor);
-		Exporter.export(hospitalVisitPatient, new FileWriter("output3.csv"), settings_hospital);
+		Exporter.export(patientWebsite, new FileWriter("output.csv"), files.get(0).getSettings());
+		Exporter.export(statsensor, new FileWriter("output2.csv"), files.get(1).getSettings());
+		Exporter.export(hospitalVisitPatient, new FileWriter("output3.csv"), files.get(2).getSettings());
 		System.out.println("Demo finished!");
+		//DEMO END
+		
+		System.exit(0);
+	}
+	
+	/**
+	 * Parses command line arguments
+	 * @param argv String[] containing all arguments.
+	 */
+	public static void parseCommandline(String[] argv){
+		int argc = argv.length;
+		for(int i = 0; i < argc; i++){
+			if(argv[i].equals("-f") && argc-i > 2){
+				String filepath = argv[i+1];
+				String settingsfilepath = argv[i+2];
+				
+				File file = new File(filepath);
+				File settings = new File(settingsfilepath);
+				
+				if(!file.exists()){
+					System.out.println(filepath + " does not exist!");
+					System.exit(1);
+				}
+				if(!settings.exists()){
+					System.out.println(settings + " does not exist!");
+					System.exit(1);
+				}
+				
+				DataFile f = new DataFile(file, settings);
+				files.add(f);
+				i = i + 2;
+			}
+			else if(argv[i].equals("-s") && argc-i > 1){
+				scriptFile = new File(argv[i+1]);
+			}
+			else{
+				String usage =
+						  "Error in program arguments.\n"
+						+ "Available commands are:\n"
+						+ "    -f <data file> <settings file>\n"
+						+ "    -s <script file>\n";
+				System.out.println(usage);
+				System.exit(1);
+			}
+		}
+	}
+	
+	/**
+	 * openGUI will run the graphical user interface.
+	 */
+	public static void openGUI(){
+		String windowTitle = "Contextproject 2015 Groep 5/E";
+	    mainUI dialog = new mainUI();
+	    dialog.pack();
+	    dialog.setTitle(windowTitle);
+		dialog.centreWindow();
+	    dialog.setVisible(true);
+
+		files = dialog.getFiles();
+		scriptFile = dialog.getScriptFile();
+		outputDir = dialog.getOutputDir();
+
+		if(files == null || scriptFile == null || outputDir == null){
+			System.out.println("Error retrieving data from gui.");
+			System.exit(1);
+		}
+
+		System.out.println("GUI done\nscriptFile = " + scriptFile.getPath() + "\noutputDir = " + outputDir.getPath() + "\nfiles = ");
+		for(DataFile file : files){
+			System.out.println(file.toString());
+		}
 	}
 }
