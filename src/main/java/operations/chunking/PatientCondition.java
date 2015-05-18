cebef05e7250db359bb16e450477d5ed22f4760f
paulbakker65 <paulbakker65@gmail.com>
2015-05-18 09:29:07 +0200

    Only commit the changed values
    To look at the individual steps, please look at branch
    chunking_with_table

---
+++ src/main/java/operations/chunking/PatientCondition.java
@@ -0,0 +1,18 @@
+package operations.chunking;
+
+import parsers.NumberValue;
+import parsers.Value;
+
+public class PatientCondition extends ChunkCondition {
+
+	@Override
+	public boolean matches(Value recordValue, Value check) {
+		NumberValue current = (NumberValue) check;
+		NumberValue record = (NumberValue) recordValue;
+		if (current.equals(record)) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ src/main/java/operations/chunking/YearCondition.java
@@ -0,0 +1,23 @@
+package operations.chunking;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import parsers.DateValue;
+import parsers.Value;
+
+public class YearCondition extends ChunkCondition {
+
+	@Override
+	public boolean matches(Value recordValue, Value check) {
+		DateValue current = (DateValue) check;
+		GregorianCalendar currentDate = current.getValue();
+		DateValue record = (DateValue) recordValue;
+		GregorianCalendar recordDate = record.getValue();
+		if (recordDate.get(Calendar.YEAR) == currentDate.get(Calendar.YEAR)) {
+			return true;
+		}
+		return false;
+	}
+
+}
