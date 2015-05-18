489052031794d9be0ddf8cc42d8efedb4b708698
paulbakker65 <paulbakker65@gmail.com>
2015-05-18 12:17:14 +0200

    Exporter now uses a Set of Strings
    No more settings.

--- src/main/java/export/Exporter.java
+++ src/main/java/export/Exporter.java
@@ -1,12 +1,10 @@
 package export;
 
-import input.Column;
-import input.Settings;
-
 import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import parsers.Value;
 import table.Record;
@@ -20,34 +18,31 @@ import com.opencsv.CSVWriter;
  */
 public class Exporter {
 
-  public static void export(Table db, Writer writer, Settings settings) throws IOException {
-    List<Column> temp = settings.getColumns(); // Get all the columns
-    List<String> columns = new ArrayList<String>();
-    for (Column col : temp) {
-      columns.add(col.getName());
-    }
+	public static void export(Table db, Writer writer, Set<String> colNames)
+			throws IOException {
+	
 
-    CSVWriter csvwriter = new CSVWriter(writer, settings.getDelimiter().charAt(0));
-    csvwriter.writeNext(columns.toArray(new String[columns.size()])); // write
-    // column
-    // names
-    for (Record dr : db) {
-      csvwriter.writeNext(generateRow(dr, columns));
-    }
-    csvwriter.close();
-  }
+		CSVWriter csvwriter = new CSVWriter(writer, ";"
+				.charAt(0));
+		csvwriter.writeNext(colNames.toArray(new String[colNames.size()])); // write
+																			// column
+																			// names
+		for (Record dr : db) {
+			csvwriter.writeNext(generateRow(dr, colNames));
+		}
+		csvwriter.close();
+	}
 
-  public static String[] generateRow(Record dr, List<String> columns) {
-    List<String> items = new ArrayList<String>();
-    for (String column : columns) {
-      Value value = dr.get(column);
-      if (value == null) {
-        items.add("");
-      } else {
-        items.add(value.toString());
-      }
-    }
-    return items.toArray(new String[items.size()]);
-  }
+	public static String[] generateRow(Record dr, Set<String> columns) {
+		List<String> items = new ArrayList<String>();
+		for (String column : columns) {
+			Value value = dr.get(column);
+			if (value == null)
+				items.add("");
+			else
+				items.add(value.toString());
+		}
+		return items.toArray(new String[items.size()]);
+	}
 
-}
+
