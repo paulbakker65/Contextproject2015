8ac1b4022fefa4e104ab948b7f46568b6bc1f954
Jan van Rosmalen <jan@janvanrosmalen.nl>
2015-05-20 11:51:23 +0200

    Fixed import warning :P

--- src/test/java/main/InputTest.java
+++ src/test/java/main/InputTest.java
@@ -1,9 +1,11 @@
 package main;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
