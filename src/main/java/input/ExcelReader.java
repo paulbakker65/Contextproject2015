00ec94cf3b14946c4988ae921948e510ce7530b5
Jan van Rosmalen <jan@janvanrosmalen.nl>
2015-05-27 12:09:34 +0200

    Fixed Excel reader. Warning! needs a lot of memory when reading large XLSX

--- src/main/java/input/ExcelReader.java
+++ src/main/java/input/ExcelReader.java
@@ -16,6 +16,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * Reads the first sheet of an XLSX 2007 File.
+ * 
  * @author unset
  *
  */
@@ -23,39 +24,60 @@ public class ExcelReader extends Reader {
 
   Iterator<Row> rowIterator;
   XSSFWorkbook workbook;
-  
+
   /**
    * Creates a new XLS reader.
-   * @param path path to the file
-   * @param sheetindex sheet number (0-based)
+   * 
+   * @param path
+   *          path to the file
+   * @param sheetindex
+   *          sheet number (0-based)
    * @throws IOException
    */
-  public ExcelReader(String path, int sheetindex) throws IOException{
+  public ExcelReader(String path, int sheetindex) throws IOException {
     super(path);
+    System.setProperty("org.apache.poi.util.POILogger", "org.apache.commons.logging.impl.NoOpLog");
     workbook = new XSSFWorkbook(filepath);
     XSSFSheet sheet = workbook.getSheetAt(sheetindex);
-    rowIterator = sheet.iterator();
+    rowIterator = sheet.rowIterator();
   }
-  
+
   @Override
   public String[] readRow() throws IOException {
-    if (rowIterator.hasNext()){
-      List<String> values = new ArrayList<String>();
-      for (Cell c :rowIterator.next()){
-        //Change to ISO if formatted as date
-        if (c.getCellType() == Cell.CELL_TYPE_NUMERIC && DateUtil.isCellDateFormatted(c)){
-          DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-          Date d = c.getDateCellValue();
-          values.add(df.format(d));
-        }
-        else {
-          values.add(c.toString());
-        }
-      }
-      return values.toArray(new String[0]);
-    }
-    else {
+    if (rowIterator.hasNext()) {
+      System.out.println();
+      Row r = rowIterator.next();
+      return rowToStrings(r);
+    } else {
       return null;
+    }
+  }
+
+  private static String[] rowToStrings(Row r) {
+    List<String> values = new ArrayList<String>();
+    int pos = 0;
+    for (Cell c : r) {
+      while (c.getColumnIndex() > pos) {
+        pos++;
+        values.add("");
+        System.out.print("-");
+      }
+      System.out.print("*");
+      values.add(cellToString(c));
+      pos++;
+    }
+    return values.toArray(new String[0]);
+  }
+
+  private static String cellToString(Cell c) {
+    if (c == null) {
+      return "";
+    } else if (c.getCellType() == Cell.CELL_TYPE_NUMERIC && DateUtil.isCellDateFormatted(c)) {
+      DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+      Date d = c.getDateCellValue();
+      return df.format(d);
+    } else {
+      return c.toString();
     }
   }
 
