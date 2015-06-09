66fffc28708b8f81794a2a130d888dee7b0b4874
Julian Hols <jthols@users.noreply.github.com>
2015-06-09 10:07:46 +0200

    Table GUI now resizable and scrollable.

--- src/main/java/main/DisplayTableGui.java
+++ src/main/java/main/DisplayTableGui.java
@@ -3,14 +3,10 @@ package main;
 import table.Table;
 
 import java.awt.BorderLayout;
-import java.awt.Dialog.ModalityType;
 import java.awt.Dimension;
-import java.awt.Insets;
-import java.awt.Toolkit;
 
-import javax.swing.BorderFactory;
 import javax.swing.JComponent;
-import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -25,43 +21,23 @@ public class DisplayTableGui extends JPanel {
 
   /**
    * Creates a gui for the given table.
-   * @param table
-   *          the table to display.
+   * @param table The table to display.
    */
   public static void init(Table table) {
-    // JFrame frame = new JFrame("Table preview: " + table.getName());
-    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JFrame frame = new JFrame("Table preview: " + table.getName());
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-    JDialog dialog = new JDialog(null, ModalityType.TOOLKIT_MODAL);
-    dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-    dialog.setTitle("Table preview: " + table.getName());
-
-    // Create and set up the content pane.
     JComponent contentPane = new DisplayTableGui(table);
-    contentPane.setOpaque(true); // content panes must be opaque
+    contentPane.setOpaque(true);
+    contentPane.setPreferredSize(new Dimension(1024, 720));
+    frame.setContentPane(contentPane);
+    
+    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-    Dimension scrSize = Toolkit.getDefaultToolkit().getScreenSize();
-    Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(dialog.getGraphicsConfiguration());
-
-    Dimension preferredDimension = null;
-
-    if (scnMax.bottom != 0) {
-      preferredDimension = new Dimension(scrSize.width, scrSize.height - scnMax.bottom - 2 * 20);
-    } else if (scnMax.top != 0) {
-      preferredDimension = new Dimension(scrSize.width, scrSize.height - scnMax.top - 2 * 20);
-    } else if (scnMax.left != 0) {
-      preferredDimension = new Dimension(scrSize.width - scnMax.left - 2 * 20, scrSize.height);
-    } else if (scnMax.right != 0) {
-      preferredDimension = new Dimension(scrSize.width - scnMax.right - 2 * 20, scrSize.height);
-    }
-
-    contentPane.setPreferredSize(preferredDimension);
-    dialog.setContentPane(contentPane);
-    dialog.setLocation(0, 0);
-
-    dialog.pack();
-    GUI.setIconImage(dialog);
-    dialog.setVisible(true);
+    frame.pack();
+    GUI.setIconImage(frame);
+    GUI.centreWindow(frame);
+    frame.setVisible(true);
   }
 
   /**
@@ -74,9 +50,9 @@ public class DisplayTableGui extends JPanel {
     super(new BorderLayout());
 
     jtable = new JTable(new DisplayTableModel(table));
+    jtable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-    add(new JScrollPane(jtable), BorderLayout.CENTER);
-    setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    add(new JScrollPane(jtable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
   }
-
 }
