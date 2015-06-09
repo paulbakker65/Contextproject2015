698b28411e64ac19d31d37c2a055ea4a84d93438
Robin Borst <robinborst95@gmail.com>
2015-06-09 09:24:47 +0200

    Added a class to read and write Table objects.

---
+++ src/test/java/table/TableFileTest.java
@@ -0,0 +1,54 @@
+package table;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import table.value.StringValue;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Class for testing reading and writing Tables.
+ */
+public class TableFileTest {
+  private Table table;
+
+  /**
+   * Initialize a Table.
+   */
+  @Before
+  public void setUp() {
+    table = new Table();
+    Record record = new Record();
+    record.put("fruit", new StringValue("banaan"));
+    record.put("groente", new StringValue("prei"));
+    table.add(record);
+  }
+  
+  @Test
+  public void testWriteReadTablePath() throws IOException, ClassNotFoundException {    
+    TableFile.writeTable(table, "medical/testTable");    
+    Table returnTable = TableFile.readTable("medical/testTable");
+    
+    assertEquals(table, returnTable);    
+  }
+  
+  @Test
+  public void testWriteReadTableFile() throws IOException, ClassNotFoundException {    
+    TableFile.writeTable(table, "medical/testTable");    
+    Table returnTable = TableFile.readTable(new File("medical/testTable.ser"));
+    
+    assertEquals(table, returnTable);    
+  }
+  
+  @Test
+  public void testWriteReadTablePathSer() throws IOException, ClassNotFoundException {    
+    TableFile.writeTable(table, "medical/testTable.ser");    
+    Table returnTable = TableFile.readTable("medical/testTable.ser");
+    
+    assertEquals(table, returnTable);    
+  }  
+}
