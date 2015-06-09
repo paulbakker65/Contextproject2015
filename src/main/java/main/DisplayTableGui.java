5da58201723753e9f50f99c52a3ea6f96bb31e3b
Julian Hols <jthols@users.noreply.github.com>
2015-06-09 10:50:27 +0200

    Columns now get sized to match their content.

--- src/main/java/main/DisplayTableGui.java
+++ src/main/java/main/DisplayTableGui.java
@@ -3,6 +3,7 @@ package main;
 import table.Table;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JComponent;
@@ -10,6 +11,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 /**
  * A GUI to display a Table.
@@ -52,7 +56,57 @@ public class DisplayTableGui extends JPanel {
     jtable = new JTable(new DisplayTableModel(table));
     jtable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
+    packColumns(jtable);
+    
     add(new JScrollPane(jtable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
         JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
+  }
+  
+  /**
+   * Packs all columns of the table to their content size.
+   * @param table The table who's columns will be packed.
+   */
+  public static void packColumns(JTable table) {
+    for (int column = 0; column < table.getColumnCount(); column++) {
+      packColumn(table, column);
+    }
+  }
+  
+  /**
+   * Packs a column to its content size.
+   * @param table A JTable containing the column.
+   * @param column The column index.
+   */
+  public static void packColumn(JTable table, int column) {
+    final TableColumnModel columnModel = table.getColumnModel();
+    final TableColumn tablecolumn = columnModel.getColumn(column);
+
+    int width = tablecolumn.getMinWidth();
+    width = Math.max(getColumnHeaderWidth(table, tablecolumn), width);
+    width = Math.max(getColumnCellsWidth(table, column), width);
+        
+    tablecolumn.setPreferredWidth(width + 1);
+  }
+  
+  private static int getColumnHeaderWidth(JTable table, TableColumn tablecolumn) {
+    TableCellRenderer renderer = tablecolumn.getHeaderRenderer();
+    if (renderer == null) {
+      renderer = table.getTableHeader().getDefaultRenderer();
+    }
+    Component component = renderer.getTableCellRendererComponent(table, 
+        tablecolumn.getHeaderValue(), false, false, 0, 0);
+    return component.getPreferredSize().width;
+  }
+  
+  private static int getColumnCellsWidth(JTable table, int column) {
+    TableCellRenderer renderer;
+    Component component;
+    int width = 0;
+    for (int row = 0; row < table.getRowCount(); row++) {
+      renderer = table.getCellRenderer(row, column);
+      component = table.prepareRenderer(renderer, row, column);
+      width = Math.max(component.getPreferredSize().width, width);
+    }
+    return width;
   }
 }
