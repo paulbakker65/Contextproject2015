5a72628040ac9735fe4db19de6e4abe020eaee5f
Paul Bakker <paulbakker@wlan-145-94-198-238.wlan.tudelft.nl>
2015-06-19 12:31:51 +0200

    ExcelReaderTest now has 100% branch coverage

--- input/src/test/java/net/tudelft/hi/e/input/ExcelReaderTest.java
+++ input/src/test/java/net/tudelft/hi/e/input/ExcelReaderTest.java
@@ -1,6 +1,7 @@
 package net.tudelft.hi.e.input;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import net.tudelft.hi.e.data.DateValue;
 import net.tudelft.hi.e.data.Table;
@@ -17,18 +18,25 @@ import java.util.GregorianCalendar;
  */
 public class ExcelReaderTest {
 
+  private Table table;
+  private DataFile df;
+  
+  /**
+   * Sets up the datafile needed for testing the ExcelReader.class.
+   * @throws Exception if anything goes wrong
+   */
   @Before
-  public void setUp() throws Exception {}
-
-  @Test
-  public void test() throws Exception {
+  public void setUp() throws Exception {
     final String datafilename = "src/test/resources/website_test2.xlsx";
     final String settingsfilename = "src/test/resources/settings_website.xml";
 
-    final DataFile df = new DataFile(new File(datafilename), new File(settingsfilename));
+    df = new DataFile(new File(datafilename), new File(settingsfilename));
+  }
 
-    final Table table = df.getParser().parse(df.getReader());
-
+  @Test
+  public void test() throws Exception {
+    table = df.getParser().parse(df.getReader());
+    
     assertEquals(57, table.size());
 
     assertEquals("Ochtend", table.get(56).get("Moment").toString());
@@ -38,5 +46,16 @@ public class ExcelReaderTest {
     assertEquals(2011, date.get(Calendar.YEAR));
     assertEquals(5, date.get(Calendar.DAY_OF_MONTH));
     assertEquals(5, date.get(Calendar.MONTH));
+    
+    df.getReader().close();
+    table = df.getParser().parse(df.getReader());
+    
+    assertTrue(table.isEmpty());
+  }
+  
+  @Test
+  public void testEmptyWorkbook() throws Exception {
+    df.getReader().close();
+    test();  
   }
 }
